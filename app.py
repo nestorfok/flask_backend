f66a2fc4c0a2e513e24f107baafe526218669b7f
fossand <fossand@amazon.com>
2020-08-08 12:20:45 +0100

    Added family information

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return "Hello Nestor!"
+def home():
+    title = "Nestor's Website"
+    family_members = ["Richard", "Fiona", "Andrew", "Nestor", "Mama"]
+    return render_template("home.html", title=title, family_members=family_members)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
